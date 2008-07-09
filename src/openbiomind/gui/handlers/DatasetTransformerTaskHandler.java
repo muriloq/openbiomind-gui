a222d3d7679bb96de4e7ceb6cd77735a1c5a177a
bsanghvi <bsanghvi@449f5d58-264e-0410-98c9-a7ff8acb955f>
2008-07-09 20:47:51 +0000

    Refactored the way wizards are open.

--- src/openbiomind/gui/handlers/DatasetTransformerTaskHandler.java
+++ src/openbiomind/gui/handlers/DatasetTransformerTaskHandler.java
@@ -11,16 +11,13 @@ import openbiomind.gui.wizards.DatasetTransformerWizard;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * The class DatasetTransformerHandler.
  *
  * @author bsanghvi
  * @since Jun 9, 2008
- * @version Jul 2, 2008
+ * @version Jul 9, 2008
  */
 public class DatasetTransformerTaskHandler extends AbstractTaskHandler {
 
@@ -29,15 +26,7 @@ public class DatasetTransformerTaskHandler extends AbstractTaskHandler {
     */
    @Override
    public Object execute(final ExecutionEvent event) throws ExecutionException {
-      final Shell shell = HandlerUtil.getActiveWorkbenchWindowChecked(event).getShell();
-
-      if (validate(shell)) {
-         final DatasetTransformerWizard datasetTransformerWizard = new DatasetTransformerWizard();
-         final WizardDialog wizardDialog = new WizardDialog(shell, datasetTransformerWizard);
-         wizardDialog.open();
-      }
-
-      return null;
+      return execute(event, new DatasetTransformerWizard());
    }
 
 }
